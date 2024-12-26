4bc7c7e5c0da20fcf85bf926160f169766fd769a
devopengin <152427505+devopengin@users.noreply.github.com>
2024-12-26 18:58:42 +0300

    Add files via upload

---
+++ АИСД/lab5/Lab05/Lab05.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+#define INF 9999
+#define VERTEX 8
+
+
+int G[VERTEX][VERTEX] = {
+	{0, 2, 0, 8, 2, 0, 0, 0},
+	{2, 0, 3, 10, 5, 0, 0, 0},
+	{0, 3, 0, 0, 12, 0, 0, 7},
+	{8, 10, 0, 0, 14, 3, 1, 0},
+	{2, 5, 12, 14, 0, 11, 4, 8},
+	{0, 0, 0, 3, 11, 0, 6, 0},
+	{0, 0, 0, 1, 4, 6, 0, 9},
+	{0, 0, 7, 0, 8, 0, 9, 0}
+};
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int quantity_edge;
+	int selected[VERTEX];
+	memset(selected, false, sizeof(selected));
+	quantity_edge = 0;
+
+	int start_vertex;
+
+	cout << "Введите начальную вершину (от 1 до " << VERTEX << "): ";
+	while (!(cin >> start_vertex) || start_vertex < 1 || start_vertex > VERTEX) {
+		cout << "Ошибка: введите целое число от 1 до " << VERTEX << ": ";
+		cin.clear();
+		cin.ignore(10000, '\n');
+	}
+
+	start_vertex -= 1;
+	selected[start_vertex] = true;
+	int x;
+	int y;
+	while (quantity_edge < VERTEX - 1) {
+		int min = INF;
+		x = 0;
+		y = 0;
+		for (int i = 0; i < VERTEX; i++) {
+			if (selected[i]) {
+				for (int j = 0; j < VERTEX; j++) {
+					if (!selected[j] && G[i][j]) {
+						if (min > G[i][j]) {
+							min = G[i][j];
+							x = i;
+							y = j;
+						}
+					}
+				}
+			}
+		}
+		cout << (x + 1) << " - " << (y + 1) << " == " << G[x][y] << endl;
+		selected[y] = true;
+		quantity_edge++;
+	}
+
+	return 0;
+}
---
+++ АИСД/lab5/lab5-2/lab5-2.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+using namespace std;
+
+#define INF 9999
+#define VERTEX 8
+int parent[VERTEX];
+
+int find(int i) {
+	if (parent[i] != i) {
+		parent[i] = find(parent[i]);
+	}
+	return parent[i];
+}
+
+void unionSets(int a, int b) {
+	parent[find(a)] = find(b);
+}
+
+void kruskal(int graph[VERTEX][VERTEX]) {
+	int edgesCount = 0;
+	for (int i = 0; i < VERTEX; i++) {
+		parent[i] = i;
+	}
+
+	while (edgesCount < VERTEX - 1) {
+		int minWeight = INF;
+		int u = -1, v = -1;
+
+		for (int i = 0; i < VERTEX; i++) {
+			for (int j = 0; j < VERTEX; j++) {
+				if (graph[i][j] > 0 && graph[i][j] < minWeight) {
+					if (find(i) != find(j)) {
+						minWeight = graph[i][j];
+						u = i;
+						v = j;
+					}
+				}
+			}
+		}
+
+		unionSets(u, v);
+		edgesCount++;
+		cout << u + 1 << " - " << v + 1 << " == " << minWeight << endl;
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int graph[VERTEX][VERTEX] = {
+		{0, 2, 0, 8, 2, 0, 0, 0},
+		{2, 0, 3, 10, 5, 0, 0, 0},
+		{0, 3, 0, 0,  12, 0, 0, 7},
+		{8, 10, 0, 0,14, 3, 1, 0},
+		{2, 5, 12, 14, 0, 11, 4, 8},
+		{0, 0, 0, 3, 11, 0, 6, 0},
+		{0, 0, 0, 1, 4, 6, 0, 9},
+		{0, 0, 7, 0, 8, 0, 9, 0}
+	};
+
+	cout << "Алгоритм Краскала: " << endl;
+	cout << "Рёбра: " << endl;
+	kruskal(graph);
+	return 0;
+}
